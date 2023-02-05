1c5be3e8da93b6e7bf20cf8a860329604ef75f0c
Rene Jr <84996527+renecfjunior@users.noreply.github.com>
2023-02-05 19:09:21 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,65 +6,69 @@ const resultado = document.querySelector('#resultado');
 const maxPeso = 300;
 const maxAltura = 3;
 
+const inputs = document.querySelectorAll('input');
 
-function isEmptyOrInvalid(){
-   if(nome.value == ''){
-      resultado.textContent = `Preencha todos os campos!`;
-   } else if(peso.value == ''){
+inputs.forEach(input => {
+  input.addEventListener('keyup', event => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      const nextInput = input.nextElementSibling;
+      if (nextInput) {
+        nextInput.focus();
+      }
+    }
+  });
+});
+
+function isEmptyOrInvalid() {
+  if (nome.value === '') {
     resultado.textContent = `Preencha todos os campos!`;
-   } else if(altura.value == '')
-   resultado.textContent = `Preencha todos os campos!`;
+  } else if (peso.value === '') {
+    resultado.textContent = `Preencha todos os campos!`;
+  } else if (altura.value === '') {
+    resultado.textContent = `Preencha todos os campos!`;
+  }
 
-   if(peso.value > maxPeso){
-     resultado.textContent = 'Peso inválido!';
-   }
+  if (peso.value > maxPeso) {
+    resultado.textContent = 'Peso inválido!';
+  }
 
-   if(altura.value > maxAltura){
-     resultado.textContent = 'Altura inválida!';
-   }
-  
-
+  if (altura.value > maxAltura) {
+    resultado.textContent = 'Altura inválida!';
+  }
 }
 
-function Imc(){
+function Imc() {
   const valueImc = (peso.value / (altura.value ** 2)).toFixed(1);
 
-    let classificacao = '';
-    
+  let classificacao = '';
 
-    if(valueImc <= 18.5){
-      classificacao = 'Abaixo do peso.';
-    } else if(valueImc <= 25){
-      classificacao = 'Peso ideal';
-    }else if (valueImc <= 30){
-      classificacao = 'Sobrepeso';
-  }else if (valueImc <= 35){
-      classificacao = 'Obesidade Grau 1.';
-  }else if (valueImc <= 40){
-      classificacao = 'Obesidade Grau 2';
-  }else {
-      classificacao = 'Obesidade Grau 3';
-}
-resultado.textContent = `${nome.value} seu IMC é ${valueImc} (${classificacao})`;
-   
-
+  if (valueImc <= 18.5) {
+    classificacao = 'Abaixo do peso.';
+  } else if (valueImc <= 25) {
+    classificacao = 'Peso ideal';
+  } else if (valueImc <= 30) {
+    classificacao = 'Sobrepeso';
+  } else if (valueImc <= 35) {
+    classificacao = 'Obesidade Grau 1.';
+  } else if (valueImc <= 40) {
+    classificacao = 'Obesidade Grau 2';
+  } else {
+    classificacao = 'Obesidade Grau 3';
+  }
+  resultado.textContent = `${nome.value} seu IMC é ${valueImc} (${classificacao})`;
 }
 
-// Evento para fazer o submit ao apertar enter
-peso.addEventListener("keyup", function(event) {
+peso.addEventListener('keyup', function(event) {
   if (event.keyCode === 13) {
-   event.preventDefault();
-   Imc();
-   isEmptyOrInvalid();
+    event.preventDefault();
+    Imc();
+    isEmptyOrInvalid();
   }
 });
 
-calcular.addEventListener('click', event =>{
-event.preventDefault();
-Imc();
-isEmptyOrInvalid();
+calcular.addEventListener('click', event => {
+  event.preventDefault();
+  Imc();
+  isEmptyOrInvalid();
 });
-
-
-
-
